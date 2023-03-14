2ccd8a838bc939e598e0c5fcfae672fbae65bc32
DF <dragonflybaby2.0@gmail.com>
2023-03-14 11:50:31 +0800

    didn't git add this

---
+++ Source/Game/gameBoard.h
@@ -0,0 +1,33 @@
+﻿#include "mygame.h"
+using namespace game_framework;
+
+class gameBoard : public CMovingBitmap {
+    friend gameBoard copyCatABoard(const gameBoard& copied);
+    // Since setup pos before  CGameStateRun::OnInit() crashes the game Constructors are NOT used 
+    std::vector<vector<BaseGrid*>> grids_;
+    vector<Ship*> ships_;
+    vector<BaseGrid*> ship_hit_;
+    int currently_sel_ship_ = -1;
+    int base_x_ = 0;
+    int base_y_ = 0;
+    bool is_enemy_ = false;
+
+public:
+    // shared methods & variables
+    vector<Ship*> getShip();
+    void show();
+
+    // deployment-phase methods
+    void init();
+    int getCurrSel() const;
+    void pickUpShip(const int& sel);
+    void rotateShip();
+    void dropShip(const CPoint& pt);
+    bool ifAllShipPlaced() const;
+    void gettingStart();
+    void whatEasesMyPainCannotBeCalledSteal(const gameBoard& copied);
+
+    // main-game-phase methods
+    bool beingHit(const int& x, const int& y);
+    bool ifAllShipSunk() const;
+};
