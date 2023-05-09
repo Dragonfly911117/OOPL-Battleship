cb381485446c4deecd54de0f71f6175a34bc05b3
DF <dragonflybaby2.0@gmail.com>
2023-05-09 11:55:48 +0800

    Update Buttons, use texts IN the image instead of render the texts ON the button

--- Source/Game/button.cpp
+++ Source/Game/button.cpp
@@ -13,12 +13,8 @@
 #include "buttons.h"
 
 myBtn::myBtn() {
-	this->_text = "";
 }
 
-void myBtn::setText(const string& str) {
-	this->_text = str;
-}
 
 void myBtn::pressed() {
 	this->SetFrameIndexOfBitmap(1);
@@ -32,19 +28,4 @@ void myBtn::released() {
 
 void myBtn::showBtn() {
 	this->ShowBitmap();
-	CDC* pDC = CDDraw::GetBackCDC();
-
-	pDC->SetBkMode(TRANSPARENT);
-	pDC->SetTextColor(RGB(255, 255, 255));
-
-	/* 變更字體 */
-	if (this->GetFrameIndexOfBitmap() == 0) {
-		CTextDraw::ChangeFontLog(pDC, 40, "ink free", RGB(255, 255, 255), 40);
-	} else {
-		CTextDraw::ChangeFontLog(pDC, 40, "ink free", RGB(188, 188, 188), 40);
-	}
-	// make the text is in the middle of the button's height
-	CTextDraw::Print(pDC, this->GetLeft() + 10, this->GetTop() + GetHeight() / 2, this->_text);
-
-	CDDraw::ReleaseBackCDC();
 }
