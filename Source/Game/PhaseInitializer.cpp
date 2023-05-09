85f8b6971882d36ff8dd154bd3e2798bc2a71bb9
DF <dragonflybaby2.0@gmail.com>
2023-05-09 12:04:12 +0800

    Remove the "Option" button from the menu

--- Source/Game/PhaseInitializer.cpp
+++ Source/Game/PhaseInitializer.cpp
@@ -14,29 +14,32 @@ void PhaseInitializer_global::init() {
 	this->_container.at(_cursorPos)->LoadBitmapA("Resources/cursor.bmp");
 }
 
-
 PhaseInitializer_menu::PhaseInitializer_menu(const vector<myBtn*>& objs) {
 	this->_container = objs;
 }
 
 void PhaseInitializer_menu::init() {
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 0; i < 3; ++i) {
 		this->_container.at(i)->LoadBitmapByString(this->_buttonPath[i]);
-		this->_container.at(i)->SetTopLeft(static_cast<int>((SIZE_X * 0.4)), static_cast<int>((SIZE_Y * 0.2 * (i % 4) + 200)));
+		this->_container.at(i)->SetTopLeft(static_cast<int>((SIZE_X * 0.4)), static_cast<int>((SIZE_Y * 0.2 * (i) + 200)));
+	}
+	
+	for (int i = 3; i < 7; ++i) {
+		this->_container.at(i)->LoadBitmapByString(this->_buttonPath[i]);
+		this->_container.at(i)->SetTopLeft(static_cast<int>((SIZE_X * 0.4)), static_cast<int>((SIZE_Y * 0.2 * ((i + 1) % 4) + 200)));
 	}
 }
 
-
-PhaseInitializer_placement::PhaseInitializer_placement(GameBoard* const& board1, const vector<myBtn*>& objs = {}, const int& baseX = 0) {
+PhaseInitializer_placement::PhaseInitializer_placement(GameBoard* const& board1, const vector<myBtn*>& objs = {}, const short& baseX = 0) {
 	this->_board1 = board1;
 	this->_container = objs;
 	this->_boardBaseX = baseX;
 }
 
 void PhaseInitializer_placement::init() {
-	this->_container.at(_startButtonPos)->LoadBitmapByString({ "Resources/gameStartButton.bmp", "Resources/gameStartButtonPressed.bmp" });
+	this->_container.at(_startButtonPos)->LoadBitmapByString({"Resources/gameStartButton.bmp", "Resources/gameStartButtonPressed.bmp"});
 	this->_container.at(_startButtonPos)->SetTopLeft(SIZE_X - 150 - this->_container.at(_startButtonPos)->GetWidth(), SIZE_Y - 150 - this->_container.at(_startButtonPos)->GetHeight());
-	this->_container.at(_randomBoardButtonPos)->LoadBitmapByString({ "Resources/randomBoardoButton.bmp", "Resources/randomBoardButtonPressed.bmp" });
+	this->_container.at(_randomBoardButtonPos)->LoadBitmapByString({"Resources/randomBoardoButton.bmp", "Resources/randomBoardButtonPressed.bmp"});
 	this->_container.at(_randomBoardButtonPos)->SetTopLeft(SIZE_X - 150 - this->_container.at(_randomBoardButtonPos)->GetWidth(), SIZE_Y - 150 - this->_container.at(_randomBoardButtonPos)->GetHeight() * 2 - 50);
 	this->_board1->init(_boardBaseX);
 }
@@ -45,9 +48,10 @@ PhaseInitializer_ending::PhaseInitializer_ending(const vector<myBtn*>& objs) {
 	this->_container = objs;
 }
 
-void PhaseInitializer_ending::init() { // TODO: add restart button and exit button
+void PhaseInitializer_ending::init() {
+	// TODO: add restart button and exit button
 	this->_container.at(_restartButtonPos)->LoadBitmapByString({"Resources/randomBoardoButton.bmp", "Resources/randomBoardButtonPressed.bmp"});
 	this->_container.at(_restartButtonPos)->SetTopLeft(SIZE_X / 2 - this->_container.at(_restartButtonPos)->GetWidth() / 2, SIZE_Y - 150 - this->_container.at(_restartButtonPos)->GetHeight() * 2 - 50);
-	this->_container.at(_exitButtonPos)->LoadBitmapByString({ "Resources/randomBoardoButton.bmp", "Resources/randomBoardButtonPressed.bmp" });
+	this->_container.at(_exitButtonPos)->LoadBitmapByString({"Resources/randomBoardoButton.bmp", "Resources/randomBoardButtonPressed.bmp"});
 	this->_container.at(_exitButtonPos)->SetTopLeft(SIZE_X / 2 - this->_container.at(_exitButtonPos)->GetWidth() / 2, SIZE_Y - 150 - this->_container.at(_exitButtonPos)->GetHeight());
 }
